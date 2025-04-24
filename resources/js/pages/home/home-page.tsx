81b944f24269fd72a898551267aaf4f7535f3621
Ak-ko <waterbubbles.ww6615@gmail.com>
2025-04-24 23:55:06 +0800

    add-hero-icon

--- resources/js/pages/home/home-page.tsx
+++ resources/js/pages/home/home-page.tsx
@@ -1,9 +1,12 @@
 import HeroSection from '@/components/app/hero-section';
 import Navbar from '@/components/app/nav';
+import { Head } from '@inertiajs/react';
 
 export default function HomePage() {
     return (
         <>
+            <Head title="Home" />
+
             <Navbar />
 
             <HeroSection />
