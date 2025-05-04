ca09676d2a30c3a7339122af2e87c7f2209bb78f
Ak-ko <waterbubbles.ww6615@gmail.com>
2025-05-04 17:48:39 +0800

    add-2-mascot-in-hero-section

---
+++ resources/js/components/app/hero-section-btn.tsx
@@ -0,0 +1,34 @@
+import { cn } from '@/lib/utils';
+import { Link } from '@inertiajs/react';
+
+interface PropsT {
+    href: string;
+    className?: string;
+    initialChildren: React.ReactNode;
+    hoverChildren: React.ReactNode;
+    initialColor?: string;
+}
+
+export default function HeroSectionBtn({ href, initialChildren, initialColor = 'black', hoverChildren, className }: PropsT) {
+    const buttonClass = cn(
+        'group relative min-h-[60px] w-full max-w-[240px] rounded-lg bg-black px-4 text-center text-[14.5px] text-black uppercase transition-all duration-500',
+        initialColor && `bg-${initialColor}`,
+        className,
+    );
+
+    return (
+        <Link className={buttonClass} href={href}>
+            {initialChildren && (
+                <p className="visible absolute top-1/2 left-0 h-full w-full -translate-y-[23px] text-white opacity-100 transition-all duration-500 group-hover:invisible group-hover:opacity-0 group-active:invisible group-active:opacity-0">
+                    {initialChildren}
+                </p>
+            )}
+
+            {hoverChildren && (
+                <p className="invisible absolute top-1/2 left-0 h-full w-full -translate-y-[23px] text-white opacity-0 transition-all duration-500 group-hover:visible group-hover:opacity-100 group-active:visible group-active:opacity-100">
+                    {hoverChildren}
+                </p>
+            )}
+        </Link>
+    );
+}
