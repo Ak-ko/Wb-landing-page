1b566a0faee77fe1f797825d48ad2cbfaaa588fc
Ak-ko <waterbubbles.ww6615@gmail.com>
2025-06-11 23:31:31 +0800

    add

--- resources/js/components/app/landing-footer.tsx
+++ resources/js/components/app/landing-footer.tsx
@@ -3,8 +3,11 @@ import Marquee from 'react-fast-marquee';
 export default function LandingFooter() {
     return (
         <div>
-            <Marquee gradient={false} speed={50} direction="left" className="h-[120px] overflow-hidden">
-                <img src="/assets/footer-mascot.png" />
+            <Marquee gradient={false} speed={50} direction="left" className="overflow-hidden">
+                <img src="/assets/footer-mascot.png" className="h-[240px]" />
+                <img src="/assets/footer-mascot.png" className="h-[240px]" />
+                <img src="/assets/footer-mascot.png" className="h-[240px]" />
+                <img src="/assets/footer-mascot.png" className="h-[240px]" />
             </Marquee>
         </div>
     );
